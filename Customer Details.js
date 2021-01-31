5dc18b657e7343734ebf11474379081567da9ddf
Ahalya-j <74547571+Ahalya-j@users.noreply.github.com>
2021-01-31 21:27:17 +0530

    Update Customer Details.js

--- Customer Details.js
+++ Customer Details.js
@@ -1,34 +1,40 @@
 import { LightningElement } from 'lwc';
-
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import NAME_FIELD from '@salesforce/schema/Customer.Name';
+import BILLING_ADDRESS_FIELD from '@salesforce/schema/Customer.BillingAddress__c';
+import EMAIL_FIELD from '@salesforce/schema/Customer.Email__c';
+import SHIPPING_ADDRESS_FIELD from '@salesforce/schema/Customer.ShippingAddress__c';
 export default class CustomerDetails extends LightningElement {
-    
-        
-        genericInputChange(event){
-            console.log('Customer Details => ' , event.target.customerdetails);
-            
-        }
-         lstMarkers = [];
-    zoomlevel = "1";
 
-    handleClick(){
+    @api recordId;
+    @api objectApiName;
 
-                // Get the Latitude and Longitude from Geolocation API
-                var latitude = position.coords.latitude;
-                var longitude = position.coords.longitude;
-                
-                // Add Latitude and Longitude to the markers list.
-                this.lstMarkers = [{
-                    location : {
-                        Street: '80/2,Kayyara house , Nochupully post',
-                City: 'Palakkad',
-                State: 'Kerala',
-                    },
-                    title : 'You are here'
-                }];
-                this.zoomlevel = "4";
-            });
-        }
+    genericInputChange(event){
+        console.log('Street => ' , event.target.street);
+        console.log('City => ' , event.target.city);
+        console.log('Province => ' , event.target.province);
+        console.log('Country => ' , event.target.country);
+        console.log('postal Code => ' , event.target.postalCode);
     }
+    salutationsList = [
+        { label: 'Mr.', value: 'Mr.' },
+        { label: 'Ms.', value: 'Ms.' },
+        { label: 'Mrs.', value: 'Mrs.' },
+    ];
 
-           
+    get salutationOptions() {
+        return this.salutationsList;
+}
+
+fields = [NAME_FIELD, BILLING_ADDRESS_FIELD, EMAIL_FIELD,SHIPPING_ADDRESS_FIELD,PHONE_FIELD];
+     handleSubmit(event){
+        console.log('Customer name : ',event.detail.fields.Name);
+         console.log('Customer billing address : ',event.detail.fields.BillingAddress);
+         console.log('Customer email address : ',event.detail.fields.Email);
+        console.log('Customer shipping address : ',event.detail.fields.ShippingAddress);
+        console.log('Customer Phone : ',event.detail.fields.Phone);
+        
+    }
+   
+      
 }
