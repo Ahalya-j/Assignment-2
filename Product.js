bdd6d1418eeb01996835fe07dde2e342f7b45f75
Ahalya-j <74547571+Ahalya-j@users.noreply.github.com>
2021-02-01 09:28:09 +0530

    Update Product.js

--- Product.js
+++ Product.js
@@ -3,11 +3,13 @@ import PRODUCT_NAME_FIELD from '@salesforce/schema/Orders.ProductName';
     import RPRODUCT_CODE_FIELD from '@salesforce/schema/Orders.ProductCode';
     import UNIT_PRICE_FIELD from '@salesforce/schema/Orders.UnitPrice';
     import TOTAL_PRICE_FIELD from '@salesforce/schema/Orders.TotalPrice';
+ import DRIEVR_FIELD from '@salesforce/schema/Orders.Driver';
+
 export default class DeliveryDetails extends LightningElement {
     
             @api recordId;
         @api objectApiName;
-        fields = [PRODUCT_NAME_FIELD, RPRODUCT_CODE_FIELD, UNIT_PRICE_FIELD,TOTAL_PRICE_FIELD];
+        fields = [PRODUCT_NAME_FIELD, RPRODUCT_CODE_FIELD, UNIT_PRICE_FIELD,TOTAL_PRICE_FIELD,DRIVER_FIELD];
         handleSubmit(event){
          
             console.log('Product detail : ',event.detail.fields);
@@ -15,5 +17,6 @@ export default class DeliveryDetails extends LightningElement {
             console.log('Product code : ',event.detail.fields.Code);
             console.log('Product unitprice : ',event.detail.fields.UnitPrice);
             console.log('Product totalprice : ',event.detail.fields.TotalPrice);
+             console.log('Product driver : ',event.detail.fields.Driver);
         }
 }
