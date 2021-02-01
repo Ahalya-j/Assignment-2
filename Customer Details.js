7e1b4e30a43fdc9cf90ef491a4a264783b035c59
Ahalya-j <74547571+Ahalya-j@users.noreply.github.com>
2021-02-01 09:26:12 +0530

    Update Customer Details.js

--- Customer Details.js
+++ Customer Details.js
@@ -1,30 +1,14 @@
-import { LightningElement ,api } from 'lwc';
+import { LightningElement ,api} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/Customer.Name';
-import BILLING_ADDRESS_FIELD from '@salesforce/schema/Customer.BillingAddress__c';
-import EMAIL_FIELD from '@salesforce/schema/Customer.Email__c';
-import SHIPPING_ADDRESS_FIELD from '@salesforce/schema/Customer.ShippingAddress__c';
+import BILLING_ADDRESS_FIELD from '@salesforce/schema/Customer.BillingAddress';
+import EMAIL_FIELD from '@salesforce/schema/Customer.Email';
+import SHIPPING_ADDRESS_FIELD from '@salesforce/schema/Customer.ShippingAddress';
+import PHONE_FIELD from'@salesforce/schema/Customer.Phone';
 export default class CustomerDetails extends LightningElement {
 
     @api recordId;
     @api objectApiName;
-
-    genericInputChange(event){
-        console.log('Street => ' , event.target.street);
-        console.log('City => ' , event.target.city);
-        console.log('Province => ' , event.target.province);
-        console.log('Country => ' , event.target.country);
-        console.log('postal Code => ' , event.target.postalCode);
-    }
-    salutationsList = [
-        { label: 'Mr.', value: 'Mr.' },
-        { label: 'Ms.', value: 'Ms.' },
-        { label: 'Mrs.', value: 'Mrs.' },
-    ];
-
-    get salutationOptions() {
-        return this.salutationsList;
-}
 
 fields = [NAME_FIELD, BILLING_ADDRESS_FIELD, EMAIL_FIELD,SHIPPING_ADDRESS_FIELD,PHONE_FIELD];
      handleSubmit(event){
